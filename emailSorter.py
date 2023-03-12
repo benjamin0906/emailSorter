7c6fcaabde6c8a5922ec166b70a69b6d1c1d08da
benjamin0906 <benjamin0906@gmail.com>
2023-03-12 20:28:31 +0100

    error messages have been added

--- emailSorter.py
+++ emailSorter.py
@@ -61,57 +61,65 @@ for arg in sys.argv:
             elif arg[3:] == 'amount':
                 sortingBy = 'amount'
 
+if(len(UserEmail) == 0):
+    print("No user email")
+    sys.exit(-1)
+if(len(UserPwd) == 0):
+    print("No user password")
+    sys.exit(-2)
+if(numberOfEmails == 0):
+    print("No number of emails to be sorted")
+    sys.exit(-3)
 
-if((len(UserEmail) != 0) and (len(UserPwd) != 0) and (numberOfEmails != 0)):
-    server = IMAPClient(Host, use_uid=True, ssl=True)
-    server.login(UserEmail, UserPwd)
-    select_info = server.select_folder("INBOX")
-    metaData = {'amount': 0, 'size': 0}
+server = IMAPClient(Host, use_uid=True, ssl=True)
+server.login(UserEmail, UserPwd)
+select_info = server.select_folder("INBOX")
+metaData = {'amount': 0, 'size': 0}
 
-    allMessages = fetchMessages(server, numberOfEmails)
-    allMyInfo = getMyInfo(allMessages)
-    metaData['amount'] = len(allMyInfo)
+allMessages = fetchMessages(server, numberOfEmails)
+allMyInfo = getMyInfo(allMessages)
+metaData['amount'] = len(allMyInfo)
 
-    groupedSenders = []
+groupedSenders = []
 
-    for myInfo in allMyInfo:
-        i = 0
-        while((i < len(groupedSenders)) and (groupedSenders[i]['from'] != myInfo['from'])):
-            i += 1
-        if(i == len(groupedSenders)):
-            groupedSenders.append({'from': myInfo['from'], 'amount': 1, 'size': myInfo['size']})
-        else:
-            groupedSenders[i]['amount'] += 1
-            groupedSenders[i]['size'] += myInfo['size']
+for myInfo in allMyInfo:
+    i = 0
+    while((i < len(groupedSenders)) and (groupedSenders[i]['from'] != myInfo['from'])):
+        i += 1
+    if(i == len(groupedSenders)):
+        groupedSenders.append({'from': myInfo['from'], 'amount': 1, 'size': myInfo['size']})
+    else:
+        groupedSenders[i]['amount'] += 1
+        groupedSenders[i]['size'] += myInfo['size']
 
-    # Sorting the list, and summing up the size
-    for i in range(0, len(groupedSenders)):
-        max = 0
-        maxIndex = 0
-        for j in range(i, len(groupedSenders)):
-            if(groupedSenders[j][sortingBy] > max):
-                max = groupedSenders[j][sortingBy]
-                maxIndex = j
-        temp = groupedSenders[i]
-        groupedSenders[i] = groupedSenders[maxIndex]
-        groupedSenders[maxIndex] = temp
-        metaData['size'] += groupedSenders[i]['size']
+# Sorting the list, and summing up the size
+for i in range(0, len(groupedSenders)):
+    max = 0
+    maxIndex = 0
+    for j in range(i, len(groupedSenders)):
+        if(groupedSenders[j][sortingBy] > max):
+            max = groupedSenders[j][sortingBy]
+            maxIndex = j
+    temp = groupedSenders[i]
+    groupedSenders[i] = groupedSenders[maxIndex]
+    groupedSenders[maxIndex] = temp
+    metaData['size'] += groupedSenders[i]['size']
 
-    outputStr = "Result:\n"
-    outputStr += 'Total amount: '+str(len(allMyInfo))+' pcs\n'
-    outputStr += 'Total size: '+str(round(metaData['size']/1024/1024, 4)) + ' MB\n'
-    for sender in groupedSenders:
-        t = str(round(100*sender[sortingBy]/metaData[sortingBy], 2)) + '%'
-        t = fillWithSpace(t, 8)
-        t += str(sender['amount'])+' pcs'
-        t = fillWithSpace(t, 20)
-        t += str(round(sender['size']/1024, 2))+' kB'
-        t = fillWithSpace(t, 36)
-        t += str(sender['from'], 'utf-8') + '\n'
-        outputStr += t
+outputStr = "Result:\n"
+outputStr += 'Total amount: '+str(len(allMyInfo))+' pcs\n'
+outputStr += 'Total size: '+str(round(metaData['size']/1024/1024, 4)) + ' MB\n'
+for sender in groupedSenders:
+    t = str(round(100*sender[sortingBy]/metaData[sortingBy], 2)) + '%'
+    t = fillWithSpace(t, 8)
+    t += str(sender['amount'])+' pcs'
+    t = fillWithSpace(t, 20)
+    t += str(round(sender['size']/1024, 2))+' kB'
+    t = fillWithSpace(t, 36)
+    t += str(sender['from'], 'utf-8') + '\n'
+    outputStr += t
 
-    if(len(outputFileName) != 0):
-        f = open(outputFileName, 'w')
-        f.write(outputStr)
-        f.close()
-    print(outputStr)
+if(len(outputFileName) != 0):
+    f = open(outputFileName, 'w')
+    f.write(outputStr)
+    f.close()
+print(outputStr)
